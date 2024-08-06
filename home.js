f966305f113f8530aa7d1741b0aebd11319d0f12
devarthur <arthu089@gmail.com>
2024-08-06 17:49:00 -0300

    nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome){
     return nome
 }
+
+function mostraSobreNome(SobreNome){
+    return SobreNome;
+}
+
+//finalizado o sistema da home
--- login.js
+++ login.js
@@ -2,3 +2,10 @@
 //trabalhando no login
 
 //finalizando sistema de login
+
+function login (nome, email){
+    return 'bem vindo';
+}
+
+//sistema de login completo 
+//retornamos o nome do usuario quando logado
